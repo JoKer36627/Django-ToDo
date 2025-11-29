6f6325379496e2c8067ab3e035003ca94251d302
Andrii Derkach <andriyderkach608@gmail.com>
2025-11-29 16:11:43 +0100

    Add allowed hosts Railway

--- todoservice/settings.py
+++ todoservice/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +26,7 @@ SECRET_KEY = 'django-insecure-q$ky7o7z8guz+*5-0t_ap2t=n(v=w&gmf*2xk3!w29cqbs&n10
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 # Application definition
 
 INSTALLED_APPS = [
